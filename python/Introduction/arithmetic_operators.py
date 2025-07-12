c559feeb208e4290814f540787f47f4d6f008c1a
Bilguun Sonor <bsonor9@gmail.com>
2025-07-12 10:44:24 +0800

    Added arithmetic operation solution

---
+++ python/Introduction/arithmetic_operators.py
@@ -0,0 +1,12 @@
+# The provided code stub reads two integers from input, a and b.
+# Add code to print three lines where:
+# 1. The first line contains the sum of the two numbers.
+# 2. The second line contains the difference of the two numbers (a - b).
+# 3. The third line contains the product of the two numbers.
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+print("The sum of 2 numbers: ",a + b)
+print("The difference of 2 numbers: ",a - b)
+print("The product of 2 numbers: ",a * b)
--- python/hello_world.py
+++
@@ -1,4 +0,0 @@
-# Print "Hello, World!" to the console
-
-if __name__ == '__main__':
-    print("Hello, World!")
