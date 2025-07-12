ef07f237a9fd660d660122909dc37868e93f5e87
Bilguun Sonor <bsonor9@gmail.com>
2025-07-12 15:00:01 +0800

    Solution to Python: Division

---
+++ python/Introduction/division.py
@@ -0,0 +1,8 @@
+# The provided code stub reads two integers, a and b, from input,
+# Add logic to print two lines. the first line should contain the result of integer division, a // b.
+# The second line should contain the result of float division, a / b.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Result of Integer Division: ", a // b)
+print("Result of Float Division: ", a / b)
