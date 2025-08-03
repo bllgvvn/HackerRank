5ccdcd276af616ac6b6e44ef98a3e2412b66e08f
Bilguun Sonor <bsonor9@gmail.com>
2025-08-03 17:38:04 +0800

    Solution of Find a String

---
+++ python/Strings/find_a_string.py
@@ -0,0 +1,21 @@
+# In this challenge, the user enters a string and a substring.
+# You have to print the number of times that the substring occurs in the given string.
+
+def count_substring(string, sub_string):
+    start = 0
+    end = len(sub_string)
+    
+    counter = 0
+    
+    while end <= len(string):
+        if sub_string == string[start:end]:
+            counter += 1
+        start += 1
+        end += 1
+    return counter
+if __name__ == '__main__':
+    string = input("Enter the main string: ").strip()
+    sub_string = input("Enter the substring: ").strip()
+
+    count = count_substring(string, sub_string)
+    print("Count: ", count)
