6539f33bd4e2c1f6b8f1045a87a67b33045e7d2f
Bilguun Sonor <bsonor9@gmail.com>
2025-08-06 19:48:49 +0800

    Added challenge problem

--- python/Strings/string_formatting.py
+++ python/Strings/string_formatting.py
@@ -1,3 +1,9 @@
+# Given an integer, n, print the following values for each integer i from 1 to n:
+# 1. Decimal
+# 2. Octal
+# 3. Hexadecimal (capitalized)
+# 4. Binary
+
 def print_formatted(number):
     width = len(bin(number)[2:])
     for i in range(1, number + 1):
