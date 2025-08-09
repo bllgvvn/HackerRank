7a4f4b6e5381ad0dae62f921efe6cefb32d5a72b
Bilguun Sonor <bsonor9@gmail.com>
2025-08-09 12:29:56 +0800

    Solution of The Minion Game

---
+++ python/Strings/the_minion_game.py
@@ -0,0 +1,32 @@
+# Kevin and Stuart want to play the 'The Minion Game'.
+# Game Rules:
+# - Both players are given the same string, S.
+# - Both players have to make substrings using the letters of the string S.
+# - Stuart has to make words starting with consonants.
+# - Kevin has to make words starting with vowels.
+# - The game ends when both players have made all possible substrings.
+# Scoring:
+# - A player gets +1 point for each occurence of the substring in the string S.
+
+def minion_game(string):
+    vowels = 'AEIOU'
+    kevin_score = 0
+    stuart_score = 0
+    length = len(string)
+    
+    for i in range(length):
+        if string[i] in vowels:
+            kevin_score += length - i
+        else:
+            stuart_score += length - i
+    
+    if kevin_score > stuart_score:
+        print(f'Kevin {kevin_score}')
+    elif stuart_score > kevin_score:
+        print(f'Stuart {stuart_score}')
+    else:
+        print('Draw')
+
+if __name__ == '__main__':
+    s = input('Enter a string: ')
+    minion_game(s)
