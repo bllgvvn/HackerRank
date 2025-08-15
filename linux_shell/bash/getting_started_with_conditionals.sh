ad701037168d020b787a89a8d868c5e9c51ed36f
Bilguun Sonor <bsonor9@gmail.com>
2025-08-15 23:11:46 +0800

    Added Description

--- linux_shell/bash/getting_started_with_conditionals.sh
+++ linux_shell/bash/getting_started_with_conditionals.sh
@@ -1,3 +1,7 @@
+# Read in one character from STDIN.
+# If the character is 'Y' or 'y', print 'YES'.
+# If the character is 'N' or 'n', print 'NO'.
+
 read -n 1 char
 
 if [ $char == 'Y' ] || [ $char == 'y' ]; then
