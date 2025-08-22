a782536f89fd3d6b280c060aa63ac714d7124ac6
Bilguun Sonor <bsonor9@gmail.com>
2025-08-22 14:48:16 +0800

    Solution of Getting started with conditionals

---
+++ linux_shell/Bash/getting_started_with_conditionals.sh
@@ -0,0 +1,10 @@
+# Read in one character from STDIN.
+# If the character is 'Y' or 'y' display "YES"
+# If the character is 'N' or 'n' display "NO".
+
+read -n 1 char
+if [[ $char == 'Y' || $char == 'y' ]]; then
+    echo "YES"
+elif [[ $char == 'N' || $char == 'n' ]]; then
+    echo "NO"
+fi
