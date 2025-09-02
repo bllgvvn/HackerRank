4558558f6edba18d21127cae57c929e194a5152a
Bilguun Sonor <B221870027@must.edu.mn>
2025-09-02 12:39:06 +0800

    Solution of Sort Command #3

---
+++ linux_shell/Text_Processing/sort_command_#3.sh
@@ -0,0 +1,6 @@
+# You are given a text file where each line contains a number.
+# There will be no extra characters other than the number of the newline at the end of each line.
+# Sort the lines in ascending order - so that the first line holds the numerically smallest number,
+# and the last line holds the numerically largest number.
+
+sort -n
