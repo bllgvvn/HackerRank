ef5af1cddd22cc0d4d3e5026a7d8d31d5dd1d8ec
Bilguun Sonor <B221870027@must.edu.mn>
2025-09-02 21:16:38 +0800

    Solution of Sort Command #4

---
+++ linux_shell/Text_Processing/sort_command_#4.sh
@@ -0,0 +1,6 @@
+# You are given a file of text, where each line contains a number.
+# There will be no extra characters other than the number or the newline at the end of each line.
+# Sort the numbers in descending order - - such that the first line holds the largest number and 
+# the last line holds the smallest number.
+
+sort -r -n
